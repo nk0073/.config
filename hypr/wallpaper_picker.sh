d5a3c4eb03b441df2e4ebcaad1abb63bd57bbec8
nk0073 <conclusionmerchant@gmail.com>
2025-08-07 23:47:59 -0400

    added wallpaper

--- hypr/wallpaper_picker.sh
+++ hypr/wallpaper_picker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WALLPAPER_DIR=~/Pictures/wallpapers
+WALLPAPER_DIR=~/.config/hypr/wallpaper
 SELECTED=$(find "$WALLPAPER_DIR" -type f \( -iname "*.png" -o -iname "*.png" \) | wofi --show dmenu --prompt "Wallpaper")
 
 [ -n "$SELECTED" ] && swww img "$SELECTED" --transition-type grow --transition-step 30 --transition-fps 60
